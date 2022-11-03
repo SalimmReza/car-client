d0bbcd859ed9f9cf890d9330cff45e0ec089b0d4
SalimmReza <salimnuaa@gmail.com>
2022-11-03 16:58:27 +0600

    2

---
+++ src/pages/OrderDetails.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState, useTransition } from 'react';
+
+const OrderDetails = ({ order, handleDelete, handleUpdate }) => {
+    // console.log(order)
+    const { _id, customerName, message, price, email, serviceTitle, service, status } = order
+    const [serviceDetails, setServiceDetails] = useState({})
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/services/${service}`)
+            .then(res => res.json())
+            .then(data => setServiceDetails(data))
+    }, [service])
+
+
+
+    return (
+
+        <tr>
+            <th>
+                <label>
+                    <button onClick={() => handleDelete(_id)} className="btn btn-ghost">X</button>
+                </label>
+            </th>
+            <td>
+                <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                        <div className="mask mask-squircle w-20 h-20">
+                            {
+                                serviceDetails?.img &&
+                                <img src={serviceDetails.img} alt="Avatar Tailwind CSS Component" />
+                            }
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{serviceTitle}</div>
+                        <div className="text-sm text-red-600">{price}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {customerName}
+                <br />
+                <span className="badge badge-ghost badge-sm">{email}</span>
+            </td>
+            <td>{message}</td>
+            <th>
+                <button 
+                onClick={()=> handleUpdate(_id)}
+                className="btn btn-ghost btn-xs lowercase">{
+                    status ? status : 'Pending'
+                }</button>
+            </th>
+        </tr>
+
+
+
+    );
+};
+
+export default OrderDetails;
--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,19 +1,98 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../Context/AuthProvider';
+import img from '../assets/images/checkout/checkout.png'
+import OrderDetails from './OrderDetails';
+import app from '../firebase/firebase.confic';
 
 const Orders = () => {
     const { user } = useContext(AuthContext);
-    const [orders, setOrders] = useState({});
+    const [orders, setOrders] = useState([]);
+    console.log(orders)
 
     const url = `http://localhost:5000/orders?email=${user.email}`
     useEffect(() => {
-        fetch(url)
+        fetch(`http://localhost:5000/orders?email=${user?.email}`)
             .then(res => res.json())
             .then(data => setOrders(data))
     }, [user?.email])
+
+    const handleDelete = (id) => {
+        const proceed = window.confirm('Do you want to Delete')
+        if (proceed) {
+            fetch(`http://localhost:5000/orders/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert('Deleted');
+                        const remaining = orders.filter(ordr => ordr._id !== id);
+                        setOrders(remaining);
+                    }
+                })
+
+        }
+    }
+
+    const handleUpdate = (id) => {
+        fetch(`http://localhost:5000/orders/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: 'Approved' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                const remaining = orders.filter(odr => odr._id !== id);
+                const approving = orders.find(odr => odr._id === id);
+                approving.status = 'Approved'
+                const newOrder = [...remaining, approving];
+                setOrders(newOrder);
+            })
+    }
     return (
-        <div>
-            <h1>orders{orders.length}</h1>
+        <div className='w-[80%] mx-auto'>
+
+            <div className='relative'>
+                <img className='w-full' src={img} alt="" />
+                <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-20  top-[50%]">
+                    <h1 className='text-white text-3xl font-bold'>
+                        Cart Details
+                    </h1>
+                </div>
+            </div>
+
+
+            <div className="overflow-x-auto w-full my-28">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th>
+
+                            </th>
+                            <th>Product</th>
+                            <th>Customer</th>
+                            <th>Message</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <OrderDetails
+                                key={order._id}
+                                order={order}
+                                handleDelete={handleDelete}
+                                handleUpdate={handleUpdate}
+                            ></OrderDetails>)
+                        }
+                    </tbody>
+
+
+                </table>
+            </div>
         </div>
     );
 };
